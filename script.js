c50a73f7687ad78fbfa80daa3f220b5c6ee4639d
diva-dev20 <divinechinenye77@gmail.com>
2026-06-12 15:13:03 +0100

    [my first ever commit!]

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("cta-btn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+    message.textContent = "🚀 Welcome! Your journey starts now.";
+});
